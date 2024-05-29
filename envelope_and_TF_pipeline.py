832e83a251506857098a7fdd0ed0f7844f4f3c3a
Samuel Louviot <sam.louviot@gmail.com>
2024-05-29 14:30:02 -0400

    up

--- envelope_and_TF_pipeline.py
+++ envelope_and_TF_pipeline.py
@@ -1,20 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S  python  #
 # -*- coding: utf-8 -*-
+# ===============================================================================
+# Author: Dr. Samuel Louviot, PhD
+# Institution: Nathan Kline Institute
+#              Child Mind Institute
+# Address: 140 Old Orangeburg Rd, Orangeburg, NY 10962, USA
+#          215 E 50th St, New York, NY 10022
+# Date: 2024-05-28
+# email: samuel DOT louviot AT nki DOT rfmh DOT org
+# ===============================================================================
+# LICENCE GNU GPLv3:
+# Copyright (C) 2024  Dr. Samuel Louviot, PhD
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ===============================================================================
+"""Signal Envelope and Time-Frequency extraction pipeline.
 
+This pipeline is used in the project of brain state prediction from EEG data in
+collaboration with John Hopkins University. It is used to extract the envelope 
+of the EEG signal and the time-frequency representation of the signal.
+"""
 import mne
 import bids
 import mne_bids
 from mne_bids import BIDSPath
 import os
 from pathlib import Path
-import eeg_fmri_cleaning.GradientRemover as GradientRemover
-import eeg_fmri_cleaning.utils as utils
+import eeg_research.preprocessing.tools.gradient_remover as GradientRemover
+import eeg_research.preprocessing.tools.utils as utils
 import numpy as np
 import pickle
 
-def parse_file_entities(filename):
-    file = Path(filename).name
-    basename, extension = os.path.splitext(file)
+def parse_file_entities(filename: str | os.PathLike) -> dict:
+    file_only = Path(filename).name
+    basename, extension = os.path.splitext(file_only)
     entities = dict()
     entities['extension'] = extension
     entities['suffix'] = basename.split('_')[-1]
@@ -35,78 +61,114 @@ def parse_file_entities(filename):
         entities[key] = value
     return entities
 
-class Envelope:
-    def __init__(self, raw):
+class EEGfeatures:
+    def __init__(self, raw: mne.io.Raw):
         self.raw = raw
-        self.frequencies = dict()
-    def extract_eeg_band_envelope(self):
-        self.frequencies = {
-            'delta': (0.5, 4),
-            'low_theta': (4, 6),
-            'high_theta': (6, 8),
-            'alpha': (8, 13),
-            'beta': (13, 30),
-            'low_gamma': (30, 40)
-        }
-        envelope_list = list()
-        for freqs in self.frequencies.values():
-            filtered = self.raw.copy().filter(*freqs)
-            envelope_list.append(
+        self.channel_names = raw.info['ch_names']
+        self.times = raw.times
+        self.frequencies = list()
+
+    def extract_eeg_band_envelope(self: 'EEGfeatures') -> 'EEGfeatures':
+        envelopes_list = list()
+
+        self.frequencies = [
+            (0.5, 4),
+            (4, 8),
+            (8, 13),
+            (13, 30),
+            (30, 40)
+        ]
+
+        for band in self.frequencies:
+            filtered = self.raw.copy().filter(*band)
+            envelopes_list.append(
                 filtered.copy().apply_hilbert(envelope = True).get_data()
                 )
 
-        self.envelopes = np.stack(envelope_list, axis = -1)
+        self.envelopes = np.stack(envelopes_list, axis = -1)
 
         return self
 
-    def extract_custom_envelope(self,
-                                low = 1, 
-                                high = 40, 
-                                step = 1):
+    def extract_custom_band_envelope(self: 'EEGfeatures',
+                                highest_frequency: int = 40,
+                                lowest_frequency: int = 1, 
+                                frequency_step: int = 1) -> 'EEGfeatures':
 
-        envelope_list = list()
-        for i, low_frequency in enumerate(range(low, high, step)):
-            self.frequencies[f'narrow_band_{i+1}'] = (low_frequency, low_frequency + step)
-            high_frequency = low_frequency + step
-            filtered = self.raw.copy().filter(low_frequency, high_frequency)
-            envelope_list.append(
+        temp_envelope_list = list()
+        for start_frequency in range(lowest_frequency, 
+                                     highest_frequency, 
+                                     frequency_step):
+            stop_frequency = start_frequency + frequency_step
+            self.frequencies.append((start_frequency, stop_frequency))
+            filtered = self.raw.copy().filter(start_frequency, stop_frequency)
+            temp_envelope_list.append(
                 filtered.copy().apply_hilbert(envelope = True).get_data()
                 )
-            self.envelopes = np.stack(envelope_list, axis = -1)
+            self.envelopes = np.stack(temp_envelope_list, axis = -1)
 
         return self
     
     def save(self, filename):
+        param_to_save = {
+            'channel_names': self.channel_names,
+            'times': self.times,
+            'frequencies': self.frequencies,
+            'envelopes': self.envelopes
+        }
         print(f'saving into {filename}')
         with open(filename, 'wb') as file:
-            pickle.dump(self, file)
+            pickle.dump(param_to_save, file)
 
-def create_morlet_epochs(raw):
+def measure_gradient_time(raw, print_results = True):
     gradient_trigger_name = GradientRemover.extract_gradient_trigger_name(raw)
-    channel_types = utils.map_channel_type(raw)
-    raw = utils.set_channel_types(raw, channel_types)
-    raw.set_montage('easycap-M1')
+    events, event_id = mne.events_from_annotations(raw)
+    picked_events = mne.pick_events(events, include=[event_id[gradient_trigger_name]])
+    average_time_space = np.mean(np.diff(picked_events[:,0] * raw.info['sfreq']))
+    std_time_space = np.std(np.diff(picked_events[:,0] * raw.info['sfreq']))
+    if print_results:
+        print(f'Average time space between gradient triggers: {average_time_space}')
+        print(f'Standard deviation of time space between gradient triggers: {std_time_space}')
+    return average_time_space
+
+class TimeFrequency:
+    def __init__(self, raw):
+        self.raw = raw
+        self.channel_names = raw.info['ch_names']
+        self.times = raw.times
+
+    def run_wavelets(self: 'TimeFrequency'):
+        frequencies = np.linspace(1,40,40)
+        cycles = frequencies / 2
+        self.power = self.raw.compute_tfr(freqs = frequencies, 
+                                n_cycles = cycles,
+                                method='morlet',
+                                average = False,
+                                return_itc = False,
+                                n_jobs = -1)
+        
+        return self
+    
+    def save(self, filename):
+        param_to_save = {
+            'channel_names': self.channel_names,
+            'times': self.times,
+            'power': self.power.get_data(),
+            'frequencies': self.power.freqs,
+        }
+        print(f'saving into {filename}')
+        with open(filename, 'wb') as file:
+            pickle.dump(param_to_save, file)
+
+def specific_crop(raw):
+    gradient_time = measure_gradient_time(raw, print_results = False)
+    gradient_trigger_name = GradientRemover.extract_gradient_trigger_name(raw)
     events, event_id = mne.events_from_annotations(raw)
     picked_events = mne.pick_events(events, include=[event_id[gradient_trigger_name]])
     picked_event_id = {gradient_trigger_name: event_id[gradient_trigger_name]}
-    epochs_gradient = mne.Epochs(raw, 
-                                picked_events, 
-                                picked_event_id, 
-                                tmin=0.1, 
-                                tmax=2.08, 
-                                baseline=None,
-                                event_repeated = 'drop',
-                                preload=True)
-    frequencies = np.linspace(1,40,40)
-    cycles = frequencies / 2
-    power = epochs_gradient.compute_tfr(freqs = frequencies, 
-                                        n_cycles = cycles,
-                                        method='morlet',
-                                        average = False,
-                                        return_itc = False,
-                                        n_jobs = -1)
-    
-    return power
+    start = picked_events[0][0] * raw.info['sfreq']
+    stop = picked_events[-1][0] * raw.info['sfreq'] + gradient_time
+    cropped = raw.copy().crop(start, stop)
+    return cropped
 
 def Main():
     derivatives_path = Path('/projects/EEG_FMRI/bids_eeg/BIDS/NEW/DERIVATIVES/eeg_features_extraction')
@@ -122,9 +184,10 @@ def Main():
             bids_path.mkdir()
             
             bids_path.update(description = 'MorletTFR')
-            power = create_morlet_epochs(raw)
-            saving_path = os.path.splitext(bids_path.fpath)[0] + '.hdf5'
-            power.save(saving_path, overwrite=True)
+            tf_object = TimeFrequency(raw)
+
+            saving_path = os.path.splitext(bids_path.fpath)[0] + '.pkl'
+            tf_object.run_wavelets().save(saving_path)
 
             envelope = Envelope(raw)
             bids_path.update(description = 'EEGbandsEnvelopes')
